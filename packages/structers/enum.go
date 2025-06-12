113fc174367c58e371387a9715a30e13b9cc825a
Fiqry919 <muhammadfiqry919@gmail.com>
2025-06-12 09:00:00 +0700

    update(structers): add new update enum query
    - add new update enum query
    - refactor database registry struct

--- packages/structers/enum.go
+++ packages/structers/enum.go
@@ -14,6 +14,14 @@ func (e *Enum) CreateQuery() string {
 	return fmt.Sprintf("CREATE TYPE IF NOT EXISTS %s AS ENUM ('%s')", e.Name, strings.Join(e.Values, "', '"))
 }
 
+func (e *Enum) UpdateQuery() string {
+	var queries []string
+	for _, v := range e.Values {
+		queries = append(queries, fmt.Sprintf("ALTER TYPE %s ADD VALUE IF NOT EXISTS '%s';", e.Name, v))
+	}
+	return strings.Join(queries, "\n")
+}
+
 func (e *Enum) DropQuery() string {
 	return fmt.Sprintf("DROP TYPE IF EXISTS %s", e.Name)
 }
